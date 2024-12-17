26666621f190d4a7aa08e0a2bf99aa933f7a2900
rougsig <rougsig@gmail.com>
2024-12-18 02:31:40 +0400

    Update NOZZLE_TO_PROBE_OFFSET

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1599,7 +1599,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -64, +4, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -64, +4, -3.55 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
